16a2d2c247f10a45d1a3d97aee2786cfa24c050b
Gerard Chaico Padilla <chaicopadillag@gmail.com>
2018-04-27 17:27:44 -0500

    popap espcialistas y set especialista

--- app/Mageli/app/src/main/java/com/chaicopaillag/app/mageli/Activity/CitaActivity.java
+++ app/Mageli/app/src/main/java/com/chaicopaillag/app/mageli/Activity/CitaActivity.java
@@ -2,14 +2,16 @@ package com.chaicopaillag.app.mageli.Activity;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
@@ -34,7 +36,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -42,10 +43,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class CitaActivity extends AppCompatActivity {
     private FlexboxLayout flexbox_pediatra;
     private FirebaseAuth firebaseAuth;
-    FirebaseUser User;
+    private FirebaseUser User;
     private Persona persona;
-    List<Persona>lista_pediatras;
-    DatabaseReference fire_base;
+    private ArrayList<Persona>lista_pediatras;
+    private DatabaseReference fire_base;
     private Toolbar toolbar;
     private Calendar calenda;
     private TimePickerDialog reloj;
@@ -53,10 +54,11 @@ public class CitaActivity extends AppCompatActivity {
     private int anio,mes,dia,hora,minuto;
     private EditText asunto,descripcion,fecha,mihora,numero_personas;
     private CircleImageView img_perfil_pediatra;
-    private  TextView nombre_pediatra,uid_pediatra;
+    private  TextView nombre_pediatra,uid_pediatra,correo_ped,esp_ped;
     private SwitchCompat sw_elegir_pediatra;
     private Button btn_cita;
     private AlertDialog.Builder PopapPediatras;
+    private ProgressDialog progress_carga;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,6 +86,8 @@ public class CitaActivity extends AppCompatActivity {
         img_perfil_pediatra=(CircleImageView)findViewById(R.id.img_perfil_pediatra_cita);
         nombre_pediatra=(TextView) findViewById(R.id.nombre_pediatra_cita);
         uid_pediatra=(TextView) findViewById(R.id.uid_pediatra);
+        correo_ped=(TextView) findViewById(R.id.correo_pediatra_cita);
+        esp_ped=(TextView) findViewById(R.id.especialidad_pediatra_cita);
         flexbox_pediatra=(FlexboxLayout)findViewById(R.id.fila_pediatra);
         btn_cita=(Button)findViewById(R.id.btn_cita);
         sw_elegir_pediatra=(SwitchCompat)findViewById(R.id.sw_elegir_pediatra);
@@ -127,32 +131,63 @@ public class CitaActivity extends AppCompatActivity {
             }
         });
     }
-
     private void cargar_pediatras() {
-        lista_pediatras=new ArrayList<Persona>();
+        lista_pediatras= new ArrayList<Persona>();
+        final List<String> lista = new ArrayList<>();
         PopapPediatras=new AlertDialog.Builder(CitaActivity.this);
-        PopapPediatras.setTitle(R.string.app_name);
-        Query consulta= fire_base.orderByChild("tipo_persona").equalTo(1);
+        PopapPediatras.setTitle(R.string.popap_titulo_pediatras);
+        final Query consulta= fire_base.orderByChild("tipo_persona").equalTo(2);
+        progress_cargando_pediatras();
         consulta.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot person : dataSnapshot.getChildren()){
                     persona=person.getValue(Persona.class);
                     lista_pediatras.add(persona);
+                    lista.add(persona.getNombre()+" "+persona.getApellidos()+" - "+persona.getEspecialidad());
                 }
+                PopapPediatras.setAdapter(new ArrayAdapter<>(
+                        CitaActivity.this,android.R.layout.simple_list_item_1,lista),
+                        new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        persona=new Persona();
+                        persona=lista_pediatras.get(item);
+                        flexbox_pediatra.setVisibility(View.VISIBLE);
+                        nombre_pediatra.setText(persona.getNombre()+" "+persona.getApellidos());
+                        uid_pediatra.setText(persona.getId());
+                        correo_ped.setText(persona.getCorreo());
+                        esp_ped.setText(persona.getEspecialidad());
+                    }
+                });
+                PopapPediatras.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        sw_elegir_pediatra.setChecked(false);
+                    }
+                });
+                PopapPediatras.setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        sw_elegir_pediatra.setChecked(false);
+                    }
+                });
+                progress_carga.dismiss();
+                PopapPediatras.show();
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
-
-        flexbox_pediatra.setVisibility(View.VISIBLE);
-        nombre_pediatra.setText("Pediatra Nombre");
-        uid_pediatra.setText("kdjada7da8d4a4da6d4");
     }
-
+    private void progress_cargando_pediatras() {
+        progress_carga=new ProgressDialog(CitaActivity.this,R.style.progrescolor);
+        progress_carga.setTitle(R.string.app_name);
+        progress_carga.setMessage(getString(R.string.cargando_pediatras));
+        progress_carga.setIndeterminate(true);
+        progress_carga.setCancelable(false);
+        progress_carga.show();
+    }
     private void validar_campos() {
         String _asunto,_descripcion,_fecha,_hora,_n_personas;
         _asunto=asunto.getText().toString();
