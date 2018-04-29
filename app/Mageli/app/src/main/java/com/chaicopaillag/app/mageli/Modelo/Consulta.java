e92f01c318638d4e193781c6a675faf6be0b34a7
Gerard Chaico Padilla <chaicopadillag@gmail.com>
2018-04-28 22:22:34 -0500

    model consultas , consultas adapter, consultas fragmento, consulta activity

---
+++ app/Mageli/app/src/main/java/com/chaicopaillag/app/mageli/Modelo/Consulta.java
@@ -0,0 +1,90 @@
+package com.chaicopaillag.app.mageli.Modelo;
+
+public class Consulta {
+    private String id;
+    private String asunto;
+    private String descripcion;
+    private String uid_paciente;
+    private String uid_pediatra;
+    private String fecha_registro;
+    private boolean flag_respuesta;
+    private boolean estado;
+
+    public Consulta() {
+    }
+
+    public Consulta(String id, String asunto, String descripcion, String uid_paciente, String uid_pediatra, String fecha_registro, boolean flag_respuesta, boolean estado) {
+        this.id = id;
+        this.asunto = asunto;
+        this.descripcion = descripcion;
+        this.uid_paciente = uid_paciente;
+        this.uid_pediatra = uid_pediatra;
+        this.fecha_registro = fecha_registro;
+        this.flag_respuesta = flag_respuesta;
+        this.estado = estado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUid_paciente() {
+        return uid_paciente;
+    }
+
+    public void setUid_paciente(String uid_paciente) {
+        this.uid_paciente = uid_paciente;
+    }
+
+    public String getUid_pediatra() {
+        return uid_pediatra;
+    }
+
+    public void setUid_pediatra(String uid_pediatra) {
+        this.uid_pediatra = uid_pediatra;
+    }
+
+    public String getFecha_registro() {
+        return fecha_registro;
+    }
+
+    public void setFecha_registro(String fecha_registro) {
+        this.fecha_registro = fecha_registro;
+    }
+
+    public boolean isFlag_respuesta() {
+        return flag_respuesta;
+    }
+
+    public void setFlag_respuesta(boolean flag_respuesta) {
+        this.flag_respuesta = flag_respuesta;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
